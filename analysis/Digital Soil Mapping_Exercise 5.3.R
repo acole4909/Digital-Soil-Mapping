02783737eaf8c69cf8416a07d6c6583655834b50
acole4909 <amanda.cole.personal@gmail.com>
2023-12-19 19:45:21 +0100

    second commit

---
+++ analysis/Digital Soil Mapping_Exercise 5.3.R
@@ -0,0 +1,247 @@
+##### Exercise 5: 5.3
+#### Data Loading and Preparation
+# Load bor random forest model
+rf_bor   <- readRDS(here::here("data/rf_bor_for_waterlog.100.rds"))
+df_train <- readRDS(here::here("data/cal_bor_for_waterlog.100.rds"))
+df_test  <- readRDS(here::here("data/val_bor_for_waterlog.100.rds"))
+
+# Set target
+target <- "waterlog.100"
+target <- as.factor(target)
+# Specify predictors_selected
+preds_selected <- names(df_train[, predictors_selected])
+
+cat("The target is:", target,
+    "\nThe predictors_all are:", paste0(predictors_all[1:8], sep = ", "), "...")
+
+# Split dataset into training and testing sets
+df_train <- df_full |> dplyr::filter(dataset == "calibration")
+df_test  <- df_full |> dplyr::filter(dataset == "validation")
+
+# Filter out any NA to avoid error when running a Random Forest
+df_train <- df_train |> tidyr::drop_na()
+df_test <- df_test   |> tidyr::drop_na()
+
+# A little bit of verbose output:
+n_tot <- nrow(df_train) + nrow(df_test)
+
+perc_cal <- (nrow(df_train) / n_tot) |> round(2) * 100
+perc_val <- (nrow(df_test)  / n_tot) |> round(2) * 100
+
+cat("For model training, we have a calibration / validation split of: ",
+    perc_cal, "/", perc_val, "%")
+set.seed(42)
+
+# Recipes for Train
+df_train$waterlog.100 <- as.factor(df_train$waterlog.100)
+is.factor(df_train$waterlog.100)
+pp <- recipes::recipe(waterlog.100 ~ NegO + mrvbf25 + mt_rr_y + Se_diss2m_50c + Se_TWI2m + Se_curv2m_s60 + be_gwn25_vdist + Se_MRVBF2m + lsf +
+                        Se_TWI2m_s15 + cindx10_25 + mt_td_y + Se_tpi_2m_50c + terrTextur + tsc25_40 + Se_NO2m_r500 + Se_curv2m_fmean_50c + Se_TWI2m_s60 +
+                        Se_slope50m + tsc25_18 + vdcn25 + Se_alti2m_std_50c + mt_tt_y + Se_curv50m + be_gwn25_hdist + Se_rough2m_10c + Se_curvplan2m_s60 +
+                        Se_diss2m_5c + Se_curvprof50m + Se_slope6m + Se_rough2m_5c + Se_SCA2m + Se_curvplan50m + Se_slope2m_s7 + Se_slope2m_fmean_5c +
+                        Se_slope2m_fmean_50c + Se_slope2m_s60 + Se_curv25m, data = df_train)
+
+library(caret)
+library(recipes)
+library(dplyr)
+library(tidyverse)
+?caret::train
+## Hyperparameter Tuning
+mtry_values <- c(2,3,4,5,6,7,8,9,10,12,14,16)
+min.node.size_values <- c(2,5,10,20,25)
+splitrule_values <- c("gini", "extratrees")
+mod <- caret::train(
+  pp,
+  data = df_train %>%
+    drop_na(),
+  method = "ranger",
+  trControl = trainControl(method = "cv", number = 5, savePredictions = "final"),
+  tuneGrid = expand.grid( .mtry = 9,
+                          .min.node.size = min.node.size_values,
+                          .splitrule = "gini"),
+  metric = "Accuracy",
+  replace = FALSE,
+  sample.fraction = 0.5,
+  num.trees = 50,
+  seed = 42                # for reproducibility
+)
+print(mod)
+# Best min.node.size was 10 with mtry of 9
+
+mod <- caret::train(
+  pp,
+  data = df_train %>%
+    drop_na(),
+  method = "ranger",
+  trControl = trainControl(method = "cv", number = 5, savePredictions = "final"),
+  tuneGrid = expand.grid( .mtry = mtry_values,
+                          .min.node.size = 25,
+                          .splitrule = "gini"),
+  metric = "Accuracy",
+  replace = FALSE,
+  sample.fraction = 0.5,
+  num.trees = 50,
+  seed = 42                # for reproducibility
+)
+print(mod)
+# Best mtry value was 12 with min.node.size 10
+
+mod <- caret::train(
+  pp,
+  data = df_train %>%
+    drop_na(),
+  method = "ranger",
+  trControl = trainControl(method = "cv", number = 5, savePredictions = "final"),
+  tuneGrid = expand.grid( .mtry = 12,
+                          .min.node.size = 10,
+                          .splitrule = splitrule_values),
+  metric = "Accuracy",
+  replace = FALSE,
+  sample.fraction = 0.5,
+  num.trees = 50,
+  seed = 42                # for reproducibility
+)
+print(mod)
+# Best combination of hyperparameters was mtry value=12, min.node.size=10, and splitrule=gini
+
+mod <- caret::train(
+  pp,
+  data = df_train |>
+    drop_na(),
+  method = "ranger",
+  trControl = trainControl(method = "cv", number = 5, savePredictions = "final"),
+  tuneGrid = expand.grid( .mtry = 12,
+                          .min.node.size = 10,
+                          .splitrule = "gini"),
+  metric = "Accuracy",
+  replace = FALSE,
+  sample.fraction = 0.5,
+  num.trees = 100,
+  seed = 42                # for reproducibility
+)
+print(mod)
+
+# Save relevant data for model testing.
+saveRDS(mod,
+        here::here("data/rf_mod_for_waterlog.100.rds"))
+
+saveRDS(df_train[, c(target, predictors_selected)],
+        here::here("data/cal_mod_for_waterlog.100.rds"))
+
+saveRDS(df_test[, c(target, predictors_selected)],
+        here::here("data/val_mod_for_waterlog.100.rds"))
+
+
+# Evaluation
+#### Model Analysis
+rf_mod   <- readRDS(here::here("data/rf_mod_for_waterlog.100.rds"))
+df_train <- readRDS(here::here("data/cal_bor_for_waterlog.100.rds"))
+df_test  <- readRDS(here::here("data/val_bor_for_waterlog.100.rds"))
+
+# Load area to be predicted
+raster_mask <- terra::rast(here::here("data-raw/geodata/study_area/area_to_be_mapped.tif"))
+# Turn target raster into a dataframe, 1 px = 1 cell
+df_mask <- as.data.frame(raster_mask, xy = TRUE)
+
+# Filter only for area of interest
+df_mask <- df_mask |>
+  dplyr::filter(area_to_be_mapped == 1)
+
+# Display df
+head(df_mask) |>
+  knitr::kable()
+
+files_covariates <- list.files(
+  path = here::here("data-raw/geodata/covariates/"),
+  pattern = ".tif$",
+  recursive = TRUE,
+  full.names = TRUE
+)
+
+preds_selected <- names(df_train[, predictors_selected])
+files_selected <- files_covariates[apply(sapply(X = preds_selected,
+                                                FUN = grepl,
+                                                files_covariates),
+                                         MARGIN =  1,
+                                         FUN = any)]
+
+# Load all rasters as a stack
+raster_covariates <- terra::rast(files_selected)
+
+# Get coordinates for which we want data
+df_locations <- df_mask |>
+  dplyr::select(x, y)
+
+# Extract data from covariate raster stack for all gridcells in the raster
+df_predict <- terra::extract(
+  raster_covariates,   # The raster we want to extract from
+  df_locations,        # A matrix of x and y values to extract for
+  ID = FALSE           # To not add a default ID column to the output
+)
+
+df_predict <- cbind(df_locations, df_predict) |>
+  tidyr::drop_na()  # Se_TWI2m has a small number of missing data
+
+# Make predictions for validation sites
+prediction <- predict(
+  rf_mod,           # RF model
+  newdata = df_test,   # Predictor data
+  num.threads = parallel::detectCores() - 1
+)
+
+# Save predictions to validation df
+df_test$predcv <- prediction
+
+# Classification Metrics
+Y <- df_test$waterlog.100
+Y <- as.factor(Y)
+X <- df_test$predcv
+X <- as.factor(X)
+
+conf_matrix_waterlog_rfmodcv <- caret::confusionMatrix(data=X, reference=Y, positive="1")
+conf_matrix_waterlog_rfmodcv
+mosaicplot(conf_matrix_waterlog_rfbasic$table,
+           main = "Confusion matrix")
+
+
+### Create Prediction Maps
+prediction <- predict(
+  rf_mod,              # RF model
+  newdata = df_predict,
+  num.threads = parallel::detectCores() - 1)
+
+# Attach predictions to dataframe and round them
+df_predict$prediction <- prediction
+
+# Extract dataframe with coordinates and predictions
+df_map <- df_predict |>
+  dplyr::select(x, y, prediction)
+
+# Turn dataframe into a raster
+raster_pred <- terra::rast(
+  df_map,                  # Table to be transformed
+  crs = "+init=epsg:2056", # Swiss coordinate system
+  extent = terra::ext(raster_covariates) # Prescribe same extent as predictor rasters
+)
+
+# Visualise predictions
+ggplot2::ggplot() +
+  tidyterra::geom_spatraster(data = raster_pred) +
+  ggplot2::scale_fill_viridis_c(
+    na.value = NA,
+    option = "viridis",
+    name = "Waterlog.100"
+  ) +
+  ggplot2::theme_classic() +
+  ggplot2::scale_x_continuous(expand = c(0, 0)) +
+  ggplot2::scale_y_continuous(expand = c(0, 0)) +
+  ggplot2::labs(title = "Predicted Waterlog")
+
+# Save raster as .tif file
+terra::writeRaster(
+  raster_pred,
+  "../data/ra_predicted_waterlog100.tif",
+  datatype = "FLT4S",  # FLT4S for floats, INT1U for integers (smaller file)
+  filetype = "GTiff",  # GeoTiff format
+  overwrite = TRUE     # Overwrite existing file
+)
---
+++ analysis/Digital Soil Mapping_Exercise 5.4.R
@@ -0,0 +1,126 @@
+##### Exercise 5: 5.4
+rf_prob <- ranger::ranger(
+probability = TRUE,
+classification = TRUE,
+y = df_train[, "waterlog.100"],     # target variable
+x = df_train[, predictors_selected], # Predictor variables
+seed = 42,                    # Specify the seed for randomization to reproduce the same model again
+num.threads = parallel::detectCores() - 1) # Use all but one CPU core for quick model training
+
+# Print a summary of fitted model
+print(rf_prob)
+
+# Save relevant data for model testing in the next chapter.
+saveRDS(rf_prob,
+        here::here("data/rf_prob_for_waterlog.100.rds"))
+
+saveRDS(df_train[, c(target, predictors_selected)],
+        here::here("data/cal_prob_for_waterlog.100.rds"))
+
+saveRDS(df_test[, c(target, predictors_selected)],
+        here::here("data/val_prob_for_waterlog.100.rds"))
+
+###Prob Model Analysis
+# Load random forest model
+rf_prob   <- readRDS(here::here("data/rf_prob_for_waterlog.100.rds"))
+df_train <- readRDS(here::here("data/cal_prob_for_waterlog.100.rds"))
+df_test  <- readRDS(here::here("data/val_prob_for_waterlog.100.rds"))
+
+# Load area to be predicted
+raster_mask <- terra::rast(here::here("data-raw/geodata/study_area/area_to_be_mapped.tif"))
+# Turn target raster into a dataframe, 1 px = 1 cell
+df_mask <- as.data.frame(raster_mask, xy = TRUE)
+
+# Filter only for area of interest
+df_mask <- df_mask |>
+  dplyr::filter(area_to_be_mapped == 1)
+
+# Display df
+head(df_mask) |>
+  knitr::kable()
+
+files_covariates <- list.files(
+  path = here::here("data-raw/geodata/covariates/"),
+  pattern = ".tif$",
+  recursive = TRUE,
+  full.names = TRUE
+)
+
+random_files <- sample(files_covariates, 2)
+terra::rast(random_files[1])
+terra::rast(random_files[2])
+
+# Load all rasters as a stack
+raster_covariates <- terra::rast(random_files)
+
+# Get coordinates for which we want data
+df_locations <- df_mask |>
+  dplyr::select(x, y)
+
+# Extract data from covariate raster stack for all gridcells in the raster
+df_predict <- terra::extract(
+  raster_covariates,   # The raster we want to extract from
+  df_locations,        # A matrix of x and y values to extract for
+  ID = FALSE           # To not add a default ID column to the output
+)
+
+df_predict <- cbind(df_locations, df_predict) |>
+  tidyr::drop_na()  # Se_TWI2m has a small number of missing data
+
+# Make predictions for validation sites
+prediction <- predict(
+  rf_prob,           # RF model
+  data = df_test,   # Predictor data
+  type="response",
+  num.threads = parallel::detectCores() - 1
+)
+
+# Save predictions to validation df
+df_test$pred <- prediction$predictions
+predictions <- prediction$predictions
+
+df_test$pred0.5 <- ifelse(df_test$pred[,1]>0.5, 1, 0)
+Y1 <- df_test$waterlog.100
+Y1 <- as.factor(df_test$waterlog.100)
+X1 <- df_test$pred0.5
+X1 <- as.factor(df_test$pred0.5)
+
+df_test$pred0.75 <- ifelse(df_test$pred[,1]>0.75, 1, 0)
+Y2 <- df_test$waterlog.100
+Y2 <- as.factor(df_test$waterlog.100)
+X2 <- df_test$pred0.75
+X2 <- as.factor(df_test$pred0.75)
+
+df_test$pred0.25 <- ifelse(df_test$pred[,1]>0.25, 1, 0)
+Y3 <- df_test$waterlog.100
+Y3 <- as.factor(df_test$waterlog.100)
+X3 <- df_test$pred0.25
+X3 <- as.factor(df_test$pred0.25)
+
+conf_matrix_waterlog_prob0.5 <- caret::confusionMatrix(data=X1, reference=Y1, positive="1")
+conf_matrix_waterlog_prob0.5
+# Sensitivity (TPR) for Tau=0.5 is 0.7143, FPR for Tau=0.5 is 0.1846
+conf_matrix_waterlog_prob0.75 <- caret::confusionMatrix(data=X2, reference=Y2, positive="1")
+conf_matrix_waterlog_prob0.75
+# Sensitivity (TPR) for Tau=0.75 is 0.2571, FPR for Tau=0.75 is 0.0385
+conf_matrix_waterlog_prob0.25 <- caret::confusionMatrix(data=X3, reference=Y3, positive="1")
+conf_matrix_waterlog_prob0.25
+# Sensitivity (TPR) for Tau=0.25 is 0.8571, FPR for Tau=0.25 is 0.4077
+
+## Plot ROC
+x1 <- 0.1846
+y1 <- 0.7143
+x2 <- 0.0385
+y2 <- 0.2571
+x3 <- 0.4077
+y3 <- 0.8571
+df <- data.frame(x = c(x1, x2, x3), y = c(y1, y2, y3))
+ggplot(df, aes(x = x, y = y)) +
+  geom_point()+
+  xlim(0,1)+
+  ylim(0,1)
+
+#ROC Curve
+basicplot <- ggplot(df_test, aes(d=waterlog.100, m=pred[,1])) + geom_roc()
+basicplot
+
